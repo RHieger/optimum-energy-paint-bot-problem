10940da01ea7cb90ee04abb3f7c1021c55f240db
Robert Hieger <rhieger@gmail.com>
2020-04-18 19:13:32 -0400

    Created Node package file and module stub.

---
+++ readline-experiment/app.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * app.js
+ * 
+ * Experimental code utilizing Node File System and readline
+ * modules to read and output a plain text file line by line
+ * 
+ * Robert Hieger
+ * 04/18/2020
+ * 
+*/
+
