01db3965b24769a0019619d3a609c13da84cb485
Robert Hieger <rhieger@gmail.com>
2020-04-19 01:14:44 -0400

    First successful test of readline .

--- readline-experiment/app.js
+++ readline-experiment/app.js
@@ -10,3 +10,30 @@
  * 
 */
 
+/**
+ * 
+ * REFERENCE ARTICLE:
+ * 
+ * Reading a File Line by Line in Node.js
+ * by Vasyl Lagutin
+ * 
+ * https://stackabuse.com/reading-a-file-line-by-line-in-node-js/
+ * 
+ */
+
+// REQUIRED MODULES:
+
+const readline = require('readline');
+const fs = require('fs');
+
+// Set up interface for reading file line-by-line:
+
+const lineReadInterface = readline.createInterface({
+    input: fs.createReadStream('treasure-island.txt'),
+    output: process.stdout,
+    console: false
+});
+
+lineReadInterface.on('line', (line) =>  {
+    console.log(line);
+});
