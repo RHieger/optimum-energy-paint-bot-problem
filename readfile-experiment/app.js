900f73958e1cbaf4f6c4d7c0a0bb054b1843e448
Robert Hieger <rhieger@gmail.com>
2020-04-20 19:15:44 -0400

    Created app.js stub.
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

---
+++ readfile-experiment/app.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * app.js
+ * 
+ * Experimental Node.js readfile module code
+ * 
+ * PURPOSE: Collect Beginning and endeing points in Bot Path, and
+ * generate count of squares painted.
+ * 
+ * Robert Hieger
+ * 04/20/2020
+ * 
+ */
