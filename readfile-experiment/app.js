15f86283dea3542df2f7a42c6bcb14e89eefa3f2
Robert Hieger <rhieger@gmail.com>
2020-04-22 00:38:58 -0400

    First successful test code with parsed text file.
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

--- readfile-experiment/app.js
+++ readfile-experiment/app.js
@@ -1,13 +1,36 @@
 /**
- * 
+ *
  * app.js
- * 
+ *
  * Experimental Node.js readfile module code
- * 
+ *
  * PURPOSE: Collect Beginning and endeing points in Bot Path, and
  * generate count of squares painted.
- * 
+ *
  * Robert Hieger
  * 04/20/2020
- * 
+ *
  */
+
+// REQUIRED MODULES:
+
+const fs = require('fs');
+
+const startX = 0;
+const startY = 0;
+
+const startCoord = [
+    startX,
+    startY
+];
+
+console.log(`Starting Coordinates: X=${ startX }, Y=${ startY }\n`);
+
+
+fs.readFile('directions.txt', { encoding: 'utf8' }, (err, directions) => {
+    if (err) console.error(err);
+    
+    directionData = directions.split(' ');    
+    console.log(directionData);
+
+});
