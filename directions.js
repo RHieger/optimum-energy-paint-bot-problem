c7e69a1c47dbcfb0e05447845eea58ecc5eb26c6
Robert Hieger <rhieger@gmail.com>
2020-04-25 10:07:37 -0400

    Added stub for directions.js module.
    Moved this functionality from paintbot.js.
    
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

---
+++ directions.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * directions.js
+ * 
+ * This module reads text file, converts resulting text string to array
+ * of strings, and returns the next coordinates to which the paint bot
+ * has traveled.
+ * 
+ * Robert Hieger
+ * 04/25/2020
+ * 
+ */
